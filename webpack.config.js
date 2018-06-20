8a95af2995be3cb33d947bab6d00db8bdf88510f
Chrystal Zou <chrystalzou@gmail.com>
2018-06-20 10:47:33 -0700

    Set up Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
+/* eslint-disable */
+
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
-
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: {main: './client/src/js/index.jsx'},
+  resolve: { extensions: ['.js', '.jsx'] },
   output: {
-    filename: 'overview.js',
-    path: DIST_DIR,
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env'],
-        },
-      },
-    ],
+  module:{
+      rules:[
+          {
+              test: /\.jsx?$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: "babel-loader"
+              }
+          },
+          {
+              test: /\.jsx?$/,
+              exclude: /node_modules/,
+              use: ['babel-loader', 'eslint-loader'],
+          }
+      ]
   },
 };
