393c7c03703b3b0153bde7ba246c0ebde78c8b76
Chrystal Zou <chrystalzou@gmail.com>
2018-06-28 10:27:40 -0700

    Made changes based on PR reviewer's feedback

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -12,7 +12,7 @@ const descriptionsSchema = new mongoose.Schema({
   notes: String,
   property_type: String,
   guests: Number,
-  bed: Number,
+  beds: Number,
   bedrooms: Number,
   bath: Number,
 });
@@ -36,63 +36,55 @@ db.on('error', console.log.bind(console, 'connection error:'));
 const createData = (id, title, descriptions, space, access, interactions, notes, property_type, guests, beds, bedrooms, bath) => {
   console.time("Create Data");
   let property = new Description({
-    "id" : 98,
-    "title" : "impedit ipsa quia",
-    "descriptions" : "non sequi magni",
-    "space" : "dolores",
-    "access" : "quaerat",
-    "interactions" : "in rerum laboriosam",
-    "notes" : "dicta quia ut",
-    "property_type" : "entire_house",
-    "guests" : 10,
-    "beds" : 6,
-    "bedrooms" : 5,
-    "bath" : 4,
+    "id" : id,
+    "title" : title,
+    "descriptions" : description,
+    "space" : space,
+    "access" : access,
+    "interactions" : interactions,
+    "notes" : notes,
+    "property_type" : property_type,
+    "guests" : guests,
+    "beds" : beds,
+    "bedrooms" : bedrooms,
+    "bath" : bath,
   });
+
   property.save((err) => {
     if (err) {
       console.log(err);
-    } else {
-      console.timeEnd("Create Data");
     }
   });
 };
 
 const readData = (id) => {
-  console.time("Read Data");
   Description.findOne({id: id}, (err) => {
     if (err) {
       console.log(err);
-    } else {
-      console.timeEnd("Read Data");
     }
+
   })
 };
 
 const updateData = (id, field, value) => {
-  console.time("Update Data");
   Description.find({id: id}, (err, description) => {
     if (err) {
       console.log(err);
     } else {
-      description[0].field = value;
+      description[0][field] = value;
       description[0].save((err) => {
         if (err) {
           console.log(err);
         }
-        console.timeEnd("Update Data");
       });
     }
   });
 };
 
 const deleteData = (id) => {
-  console.time("Delete Data");
-  Description.deleteOne({id: id}, (err) => {
+  Description.deleteOne({ id }, (err) => {
     if (err) {
       console.log(err);
-    } else {
-      console.timeEnd("Delete Data");
     }
   });
 };
